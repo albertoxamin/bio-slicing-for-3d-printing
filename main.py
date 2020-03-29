dcd84d9736ba4956814d8f16e3189729c6c549ac
Alberto Xamin <alberto@xamin.it>
2020-03-29 17:46:25 +0200

    custom bounder and mutator

--- main.py
+++ main.py
@@ -1,32 +1,52 @@
 #!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3.6
+import os
+import shutil
 from random import Random
 from time import time
-import inspyred
-import subprocess
-from slicing import Slicing
 
+import inspyred
+import matplotlib.pyplot
+
+from slicing import Slicing, slicing_mutation
+
+algorithm = 'ga'
+
+folder = 'gcodes/'
+for the_file in os.listdir(folder):
+    file_path = os.path.join(folder, the_file)
+    try:
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+        #elif os.path.isdir(file_path): shutil.rmtree(file_path)
+    except Exception as e:
+        print(e)
 
 def main(prng=None, display=False):
     if prng is None:
         prng = Random()
         prng.seed(time())
-    subprocess.Popen(['rm', '-f', 'gcodes/*']).wait()
-    problem = Slicing()
-    print('No evolution individual with [0, 0, 0, 0.2] has a fitness of {0}'.format(
-        problem.slice_and_get_fit([0, 0, 0, 0.2])))
-    ea = inspyred.ec.GA(prng)
-    ea.terminator = inspyred.ec.terminators.evaluation_termination
-    final_pop = ea.evolve(generator=problem.generator, evaluator=inspyred.ec.evaluators.parallel_evaluation_mp,
-                          mp_evaluator=problem.evaluator, mp_num_cpu=4,
-                          pop_size=20,
-                          maximize=problem.maximize,
-                          bounder=inspyred.ec.Bounder(0, 360),
-                          max_evaluations=200,
-                          num_elites=1)
+    problem = Slicing(stl_file='models/pole.stl')
+    print(f'No evolution individual with [0, 0, 0, 0.2] has a fitness of {problem.slice_and_get_fit([0, 0, 0, 0.2])}')
+    if algorithm == 'ga':
+        ea = inspyred.ec.GA(prng)
+        ea.terminator = [inspyred.ec.terminators.evaluation_termination, inspyred.ec.terminators.diversity_termination]
+        ea.variator = [inspyred.ec.variators.blend_crossover, slicing_mutation]
+        ea.observer = [inspyred.ec.observers.stats_observer, inspyred.ec.observers.file_observer]
+        final_pop = ea.evolve(generator=problem.generator, evaluator=inspyred.ec.evaluators.parallel_evaluation_mp,
+                            mp_evaluator=problem.evaluator, mp_num_cpu=4,
+                            pop_size=30,
+                            maximize=problem.maximize,
+                            statistics_file=open("stats.csv", "w"),
+                            individuals_file=open("inds.csv", "w"),
+                            bounder=problem.bounder,
+                            max_evaluations=1000,
+                            num_elites=3)
+
     if display:
-        best = max(final_pop)
-        problem.slice_candidate(best.candidate, 'best')
-        print('Best Solution: \n{0}'.format(str(best)))
+        best = min(final_pop)
+        problem.slice_and_get_fit(best.candidate, 'best')
+        stats = inspyred.ec.analysis.fitness_statistics(final_pop)
+        print(f"Best: {stats['best']} Worst: {stats['worst']} Mean: {stats['mean']} Std: {stats['std']}")
     return ea
 
 
--- slicing.py
+++ slicing.py
@@ -1,22 +1,43 @@
-from inspyred import benchmarks
-from inspyred.ec.emo import Pareto
+import math
+import re
+import subprocess
+import time
+import traceback
+
+import inspyred
+import numpy as np
 from inspyred.ec.variators import mutator
 from pylab import *
-import copy
-import subprocess
-import re
-import math
-import numpy as np
 
 added_hardcoded = False
 
-class Slicing():
+layer_heights = [0.12, 0.16, 0.2, 0.24, 0.28]
 
-    slic3r_path = '/Applications/PrusaSlicer.app/Contents/MacOS/PrusaSlicer'
-    stl_file = 'pikachu.stl'
-    ini_file = '/Users/alberto/Library/Application Support/PrusaSlicer/print/Anet A8.ini'
-    operation_cost = 150 * 0.0052 / 60  # electricity cost per minute
-    material_cost = 14 / 300 / 1000  # 300mt ~ 1kg, cost per mm
+class SlicingBounder(object):    
+    def __call__(self, candidate, args):
+        closest = lambda l, el : min(l, key=lambda x:abs(x-el))
+        for i, c in enumerate(candidate):
+            if i < 3:
+                candidate[i] = max(min(c, 360), 0)
+            else:
+                candidate[i] = closest(layer_heights, c)
+        return candidate
+
+@mutator    
+def slicing_mutation(random, candidate, args):
+    mut_rate = args.setdefault('mutation_rate', 0.1)
+    bounder = args['_ec'].bounder
+    mutant = candidate.copy()
+    for i, m in enumerate(mutant):
+        if random.random() < mut_rate:
+            if i < 3:
+                mutant[i] += random.gauss(0, (360) / 10.0)
+            else:
+                mutant[i] = random.choice(layer_heights)
+    mutant = bounder(mutant, args)
+    return mutant
+
+class Slicing:
 
     def generator(self, random, args):
         global added_hardcoded
@@ -24,47 +45,58 @@ class Slicing():
             added_hardcoded = True
             return [0, 0, 0, 0.2]
         individual = [random.uniform(0, 360) for i in range(3)]
-        individual.append(random.choice([0.12, 0.16, 0.2, 0.24, 0.28]))
+        individual.append(random.choice(layer_heights))
         return individual
 
-    def slice_candidate(self, candidate, name=""):
-        slicing = subprocess.Popen([self.slic3r_path, self.stl_file, '--gcode', '--rotate', str(candidate[0]),
-                                    '--rotate-x', str(candidate[1]), '--rotate-y', str(
-            candidate[2]), '--layer-height', str(candidate[3]), '--align-xy', '0,0', '--load', self.ini_file,
-            '-o', './gcodes/' + name + '.gcode'], stdout=subprocess.DEVNULL)
-        slicing.wait()
-
-    def slice_and_get_fit(self, cs, name=""):
-        if name == "":
-            name=str(id(cs))
-        self.slice_candidate(cs, name)
-        tail = subprocess.Popen(
-            ['tail', '-n150', './gcodes/' + name + '.gcode'], stdout=subprocess.PIPE)
-        sed = subprocess.Popen(
-            ['sed', '-n', '-e', '/M84/,$p'], stdin=tail.stdout, stdout=subprocess.PIPE)
-        score = subprocess.Popen(
-            ['sed', '-e', '/avoid/,$d'], stdin=sed.stdout, stdout=subprocess.PIPE)
-        score_str = str(score.communicate())
-        filament_length = float(re.findall(r"[0-9]+\.[0-9]+", score_str)[0])
+    def parse_gcode(self, gcode_str):
+        filament_length = float(re.findall(r"[0-9]+\.[0-9]+", gcode_str)[0])
         printing_time_match = re.findall(
-            r"(\d*)h{0,1} (\d+)m (\d+)s", score_str)[0]
+            r"(\d*)h{0,1} (\d+)m (\d+)s", gcode_str)[0]
         printing_time_match = np.array(printing_time_match)
         printing_time_match[printing_time_match == ''] = 0
         printing_time = int(printing_time_match[0]) * 60 + int(
             printing_time_match[1]) + int(printing_time_match[2]) / 60
-        # return filament_length * self.material_cost + printing_time * self.operation_cost
-        return filament_length
+        return filament_length * self.material_cost + printing_time * self.operation_cost
 
-    def __init__(self, constrained=False):
+    def slice_and_get_fit(self, cs, name=""):
+        if name == "":
+            name = str(id(cs))
+
+        command = f"{self.slic3r_path} {self.stl_file} --gcode --rotate {str(cs[0])} --rotate-x {str(cs[1])} --rotate-y {str(cs[2])} --layer-height {str(cs[3])} --align-xy 0,0 --load '{self.ini_file}' -o ./gcodes/{name}.gcode && cat gcodes/{name}.gcode | sed -n -e '/M84/,$p' | sed -e '/avoid/,$d'"
+        try:
+            output = subprocess.check_output(
+                command, shell=True, executable='/bin/bash', universal_newlines=True, stderr=subprocess.DEVNULL)
+            if len(re.findall(r"[0-9]+\.[0-9]+", output)) > 0:
+                return self.parse_gcode(output)
+            else:
+                command = f"cat gcodes/{name}.gcode.tmp | sed -n -e '/M84/,$p' | sed -e '/avoid/,$d'"
+                output = subprocess.check_output(
+                    command, shell=True, executable='/bin/bash', universal_newlines=True)
+                if len(re.findall(r"[0-9]+\.[0-9]+", output)) > 0:
+                    return self.parse_gcode(output)
+                else:
+                    print("File not found")
+                    return 99999999999999
+        except subprocess.CalledProcessError as slicingException:
+            print("Unprintable")
+            return 99999999999999
+
+    def __init__(self, constrained=False,
+     slic3r_path='/Applications/PrusaSlicer.app/Contents/MacOS/PrusaSlicer',
+     stl_file='models/pikachu.stl',
+     ini_file='/Users/alberto/Library/Application Support/PrusaSlicer/print/Anet A8.ini',
+     filament_kg_cost=14, kw_cost=0.052):
         self.maximize = False
         self.constrained = constrained
+        self.bounder = SlicingBounder()
+        self.slic3r_path = slic3r_path
+        self.stl_file = stl_file
+        self.ini_file = ini_file
+        self.operation_cost = 1.5 * kw_cost / 60 # electricity cost per minute, assuming a 150W printer
+        self.material_cost = filament_kg_cost / 300 / 1000  # 300mt ~ 1kg, cost per mm
 
     def evaluator(self, candidates, args):
         fitness = []
         for cs in candidates:
-            try:
-                fitness.append(self.slice_and_get_fit(cs))
-            except:
-                print(cs)
-                fitness.append(math.inf)
+            fitness.append(self.slice_and_get_fit(cs))
         return fitness
