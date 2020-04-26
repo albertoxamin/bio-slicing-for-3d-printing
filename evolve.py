2637a401877dfcddbda05306921b264567d8bfdc
Alberto Xamin <alberto@xamin.it>
2020-04-26 14:54:32 +0200

    statistical

---
+++ evolve.py
@@ -0,0 +1,34 @@
+from slicing import Slicing, slicing_mutation, hardcoded
+import inspyred
+# import inspyred.ec.observers
+
+
+
+def evolve(algorithm, problem, seeds, prng):
+    evolve_args = {
+        'generator': problem.generator,
+        'evaluator': inspyred.ec.evaluators.parallel_evaluation_mp,
+        'mp_evaluator': problem.evaluator,
+        'mp_num_cpu': 4,
+        'pop_size': 30,
+        'maximize': problem.maximize,
+        'statistics_file': open("stats.csv", "w"),
+        'individuals_file': open("inds.csv", "w"),
+        'bounder': problem.bounder,
+        'max_evaluations': 300,
+        'seeds': seeds
+    }
+    if algorithm == 'ga':
+        ea = inspyred.ec.GA(prng)
+    elif algorithm == 'eda':
+        ea = inspyred.ec.EDA(prng)
+        # evolve_args['num_selected'] = 3
+        # evolve_args['num_offspring'] = 30,
+        # evolve_args['num_elites'] = 3
+    if algorithm != 'none':
+        ea.terminator = [inspyred.ec.terminators.evaluation_termination,
+                        inspyred.ec.terminators.diversity_termination]
+        ea.variator = [
+            inspyred.ec.variators.uniform_crossover, slicing_mutation]
+        final_pop = ea.evolve(**evolve_args)
+        return final_pop
--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from random import Random
 from time import time
 
 import inspyred
-import matplotlib.pyplot
 
-from slicing import Slicing, slicing_mutation, hardcoded
+import csv
+
+from slicing import Slicing, hardcoded
+from evolve import evolve
 
 folder = 'gcodes/'
 for the_file in os.listdir(folder):
@@ -15,10 +17,17 @@ for the_file in os.listdir(folder):
     try:
         if os.path.isfile(file_path):
             os.unlink(file_path)
-        #elif os.path.isdir(file_path): shutil.rmtree(file_path)
     except Exception as e:
         print(e)
 
+def my_observer(population, num_generations, num_evaluations, args):
+    best = max(population)
+    # plt_gen.append(num_evaluations)
+    # plt_best.append(best.fitness)
+    args['fit'].append(best.fitness)
+    # print('{0:6} -- {1} : {2}'.format(num_generations, 
+                                    #   best.fitness, 
+                                    #   str(best.candidate)))
 
 def main(prng=None, display=False):
     if prng is None:
@@ -27,54 +36,52 @@ def main(prng=None, display=False):
 
     models = os.listdir('models')
     nltr = '\n  '
-    print(
-        f"Available models:{nltr}{nltr.join([f'{i}) {x}' for i, x in enumerate(models)])}")
-    model = models[int(input('Choose the model to slice: '))]
-    problem = Slicing(stl_file=f'models/{model}')
-    print("These are the default fitnesses without evolution, that equal to placing the object in the build plate and slicing it with basic rotations")
-    for c in hardcoded:
-        print(f'{c} has a cost of {problem.slice_and_get_fit(c):.3} €')
-    evolve_args = {
-        'generator': problem.generator,
-        'evaluator': inspyred.ec.evaluators.parallel_evaluation_mp,
-        'mp_evaluator': problem.evaluator,
-        'mp_num_cpu': 4,
-        'pop_size': 30,
-        'maximize': problem.maximize,
-        'statistics_file': open("stats.csv", "w"),
-        'individuals_file': open("inds.csv", "w"),
-        'bounder': problem.bounder,
-        'max_evaluations': 500,
-        'seeds': hardcoded
-    }
-    algorithm = ''
-    while algorithm not in ['ga', 'eda', 'none']:
-        algorithm = input('Which algorithm do you want to use [ga|es|eda|none]: ').lower()
-    if algorithm == 'ga':
-        ea = inspyred.ec.GA(prng)
-    elif algorithm == 'eda':
-        ea = inspyred.ec.EDA(prng)
-        evolve_args['num_selected'] = 3
-        evolve_args['num_offspring'] = 30,
-        evolve_args['num_elites'] = 3
-    if algorithm != 'none':
-        ea.terminator = [inspyred.ec.terminators.evaluation_termination,
-                         inspyred.ec.terminators.diversity_termination]
-        ea.variator = [
-            inspyred.ec.variators.uniform_crossover, slicing_mutation]
-        ea.observer = [inspyred.ec.observers.stats_observer,
-                       inspyred.ec.observers.file_observer]
-        final_pop = ea.evolve(**evolve_args)
 
-    if display:
-        best = min(final_pop)
-        problem.slice_and_get_fit(best.candidate, f'best')
-        stats = inspyred.ec.analysis.fitness_statistics(final_pop)
-        print(
-            f"Best: {stats['best']} Worst: {stats['worst']} Mean: {stats['mean']} Std: {stats['std']}")
-        if 'n' not in input('Open best gcode? [Y/n]').lower():
-            os.system('open ./gcodes/best.gcode')
-    return ea
+    print(f"Available models:{nltr}{nltr.join([f'{i}) {x}' for i, x in enumerate(models)])}")
+
+    with open('summary.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['model', 'algorithm','best_human','best_evolved', 'decrease'])
+    for model in models:
+        problem = Slicing(stl_file=f'models/{model}')
+        # print("These are the default fitnesses without evolution, that equal to placing the object in the build plate and slicing it with basic rotations")
+        best_human = 9999
+        for c in hardcoded:
+            # print(f'{c} has a cost of {problem.slice_and_get_fit(c):.3} €')
+            best_human = min(best_human, problem.slice_and_get_fit(c))
+        final_pops_fit = []
+        algorithms = ['ga', 'eda']
+        for algorithm in algorithms:
+            best_fitnesses = []
+            for i in range(5):
+                final_pop = evolve(algorithm, problem, hardcoded, prng)
+                print(f'{algorithm} run {i} completed')
+                stats = inspyred.ec.analysis.fitness_statistics(final_pop)
+                best_fitnesses.append(1 - stats['best'] / best_human)
+                problem.slice_and_get_fit(min(final_pop).candidate, f'best_{model}_{algorithm}_{i}')
+                with open('summary.csv', 'a', newline='') as file:
+                    writer = csv.writer(file)
+                    writer.writerow([model, algorithm, best_human, stats['best'], 1 - stats['best'] / best_human])
+            # final_pops_fit.append(best_fitnesses)
+            # if display:
+            #     results.append()
+                # best = min(final_pop)
+                # problem.slice_and_get_fit(best.candidate, f'best')
+                # stats = inspyred.ec.analysis.fitness_statistics(final_pop)
+                # print(
+                #     f"Best: {stats['best']} Worst: {stats['worst']} Mean: {stats['mean']} Std: {stats['std']}")
+                # plt.plot(plt_gen, plt_best)
+                # plt.show()
+                # if 'n' not in input('Open best gcode? [Y/n]').lower():
+                #     os.system('open ./gcodes/best.gcode')
+        # fig = plt.figure('Best fitness obtained during 5 runs')
+        # ax = fig.gca()
+        # ax.boxplot(final_pops_fit)
+        # ax.set_xticklabels(algorithms)
+        # ax.set_xlabel('Algorithm')
+        # ax.set_ylabel('Best fitness')
+        # plt.show()
+        # input('press to continue')
 
 
 if __name__ == '__main__':
